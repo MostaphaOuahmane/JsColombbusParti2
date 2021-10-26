f6e1ca98040404ca6d298d4d9e3d9bd88a714e4b
MostaphaOuahmane <87656171+MostaphaOuahmane@users.noreply.github.com>
2021-10-26 12:50:33 +0200

    upated e

--- 00_exos/00_exos/13-Parite.js
+++ 00_exos/00_exos/13-Parite.js
@@ -1,20 +1,36 @@
+console.log('*********** DEMO FOR ********');
 
 for (var i = 1; i<=10; i++) {
     if (i % 2 === 0) {
        console.log(i + ' est pair.');
     }
 }
-
-console.log('*******************');
-for (var i = 1; i<=20; i++) {
+// exo 1
+console.log('********* EXO 1 **********'); 
+for (var i = 1; i<=10; i++) {
     if (i % 2 === 0) {
        console.log(i + ' est pair.');
     } else {
         console.log(i + ' est impair');
     }
 }
-console.log('*******************');
 
+// exo 2 FOR
+ document.write('<p>*************** EXO 2 ***************</p>');
 
+ var nbrUtilisateur1 = parseInt(prompt('Exo 2 : rentrez un nombre entier, merci '));
+ console.log(typeof nbrUtilisateur1, nbrUtilisateur1);
 
+ var nbrUtilisteurMax = (nbrUtilisateur1 + 10);
+ console.log(typeof nbrUtilisteurMax, nbrUtilisteurMax);
 
+ for (var i = nbrUtilisateur1; i<=nbrUtilisteurMax; i++) {
+    if (i % 2 === 0) {
+       document.write('<p class="stabilo"> ' + i + ' est pair.</p>');
+    } else {
+        document.write('<p> ' + i + ' est impair.</p>');
+    }
+}
+
+// exo 3 WHILE
+document.write('<p>*************** EXO 3 ***************</p>');
