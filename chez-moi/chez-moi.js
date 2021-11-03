be096785405f720c30900d2ed81c491979b0e092
Ouahmane Mostapha <87656171+MostaphaOuahmane@users.noreply.github.com>
2021-11-03 21:09:40 +0100

    snsns

---
+++ chez-moi/chez-moi.js
@@ -0,0 +1,15 @@
+
+var solde = 5000 ,
+    solde1 = 100 ,
+    solde2 = 75 ,
+     solde3 = 25,
+     solde4 = 2134,
+     solde5 = 700;
+
+document.getElementById("prix").innerHTML = solde;
+document.getElementById("prix1").innerHTML = solde / solde1; 
+document.getElementById("prix2").innerHTML = solde - solde2; 
+document.getElementById("prix3").innerHTML = solde * solde3; 
+document.getElementById("prix4").innerHTML = solde - solde4;  
+document.getElementById("prix5").innerHTML = solde + solde5;  
+       console.log( solde ,solde4 ,solde5, solde1 , solde2, solde3);
