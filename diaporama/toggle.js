86c8c69cc416ae921037dca3dee362d0c9f7621e
Ouahmane Mostapha <87656171+MostaphaOuahmane@users.noreply.github.com>
2021-11-05 00:00:35 +0100

    ttrr;krtut

--- diaporama/toggle.js
+++
@@ -1,9 +0,0 @@
-// alert("hello toggle");
-let bouton = document.querySelector('#cacheImage');
-console.log(bouton);
-let photo =document.querySelector('.cache');
-console.log(photo);
-function cliqueBouton () {
-    photo.classList.toggle('cache')
-}
-bouton.addEventListener('click',cliqueBouton);
